4542f87160976feb35bea84f6a7a2347b044e747
Guillermo A. Perez <guillermoalberto.perez@uantwerpen.be>
2020-11-04 12:05:20 +0100

    added an option to update tags in files

--- scripts/tlsfsynth_newref.py
+++ scripts/tlsfsynth_newref.py
@@ -1,23 +1,64 @@
 #!/usr/bin/env python3
 
 """
-This script reads a StarExec job-information file given
-in CSV format and holding information about a TLSF synthesis
-job and adds to it a column with the NEW reference minimal
-implementation. It assumes that all entries in the file
-correspond to correctly synthesized outputs (no unreal
-entries). Additionally, it directly adds a score per entry
-based on the new reference.
+This script reads a StarExec job-information file given in CSV format and
+holding information about a TLSF synthesis job and adds to it a column with
+the minimal implementation (over values in the file! this ignores tags).  The
+reference value includes the number of latches + no. of gates. It assumes
+that all entries in the file correspond to correctly synthesized outputs (no
+unreal entries). Additionally, it directly adds a score per entry based on the
+new reference.
 
 Author: Guillermo A. Perez @ UAntwerp 2020-
 """
 
 import csv
+import fileinput
 import math
+import os
+import re
 import sys
 
 
-def parse_csv(filename):
+status_re = re.compile(r"^//STATUS\s*:\s*(.*)$")
+refsize_re = re.compile(r"^//REF_SIZE\s*:\s*(.*)$")
+
+
+def get_tags(filename):
+    refsize = None
+    f = open(filename, 'r')
+    for line in f:
+        res = status_re.match(line)
+        if res is not None:
+            assert(res.group(1).strip() == "realizable")
+        res = refsize_re.match(line)
+        if res is not None:
+            refsize = int(res.group(1).strip())
+            break
+    f.close()
+    return refsize
+
+
+def add_tags(filename, new_min):
+    f = open(filename, 'a')
+    f.write("\n//#!SYNTCOMP")
+    f.write("\n//STATUS : realizable")
+    f.write(f"\n//REF_SIZE : {new_min}")
+    f.write("\n//#.")
+    f.close()
+
+
+def upd_tags(filename, new_min):
+    with fileinput.FileInput(filename, inplace=True, backup='.bak') as f:
+        for line in f:
+            res = refsize_re.match(line)
+            if res is not None:
+                print(f"//REF_SIZE : {new_min}")
+            else:
+                print(line, end='')
+
+
+def parse_csv(filename, bench_root):
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -26,7 +67,7 @@ def parse_csv(filename):
             if line_count == 0:
                 pass
             else:
-                bench_name = row[1]
+                bench_name = row[1][row[1].rfind('/')+1:]
                 diff_ref = 0 if row[17] == "-" else int(row[17])
                 new_size = int(row[14]) + int(row[18])
                 init_ref = -1 * (diff_ref - new_size)
@@ -48,22 +89,50 @@ def parse_csv(filename):
             if line_count == 0:
                 print(f"{','.join(row)},Min_Ref,Score")
             else:
-                bench_name = row[1]
+                bench_name = row[1][row[1].rfind('/')+1:]
                 # below I load the sizes + 1 to avoid division by 0
-                # for stupid benchmarks which require no gates in the
-                # solution
+                # for stupid benchmarks which require no gates
+                # and no latches in the solution
                 new_size = float(int(row[14]) + int(row[18]) + 1)
                 ref_size = float(min_ref[bench_name] + 1)
                 score = 2.0 - math.log(new_size / ref_size, 10)
                 capped_score = max(0.0, score)
                 print(f"{','.join(row)},{min_ref[bench_name]},{capped_score}")
             line_count += 1
+
+    # We now explore the subdirectories to find benchmark files and update
+    # their tags
+    if bench_root is not None:
+        print(f"Starting benchmark-tag update from {bench_root}",
+              file=sys.stderr)
+        for root, dirs, files in os.walk(bench_root):
+            for name in files:
+                if name in min_ref:
+                    full_name = os.path.join(root, name)
+                    print(f"Found benchmark {full_name}", file=sys.stderr)
+                    ref_size = get_tags(full_name)
+                    if ref_size is None:
+                        print("Adding tags for the first time",
+                              file=sys.stderr)
+                        add_tags(full_name, min_ref[name])
+                    elif ref_size > min_ref[name]:
+                        print("Updating tag",
+                              file=sys.stderr)
+                        upd_tags(full_name, min_ref[name])
+
     return 0
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Expected the full path for the input file "
-              "as unique argument")
+    if len(sys.argv) < 2:
+        print("Too few arguments, expected the full path for the "
+              + "input file at least", file=sys.stderr)
         exit(1)
-    exit(parse_csv(sys.argv[1]))
+    if len(sys.argv) > 3:
+        print("Too many arguments, expected the input-file path and "
+              + "(optionally) a folder path to update tags", file=sys.stderr)
+        exit(1)
+    if len(sys.argv) == 3:
+        exit(parse_csv(sys.argv[1], sys.argv[2]))
+    else:
+        exit(parse_csv(sys.argv[1], None))
