304d59643f90f62e1d45a1856d6f4021e0eb4b4b
Guillermo A. Perez <guillermoalberto.perez@uantwerpen.be>
2022-07-18 22:15:09 +0200

    send new version tlsfreal

--- scripts/tlsfreal.py
+++ scripts/tlsfreal.py
@@ -1,52 +1,95 @@
 #!/usr/bin/env python3
-
 """
-This script reads a StarExec job-information file given in CSV format and
-holding information about a TLSF realizability jobs and collects useful
-statistics.
+This script generates the cactusplots for the TLSF tracks of
+SYNTCOMP, along with rankings and other useful information
+NOTE: It focuses on time, not on quality
 
-Author: Guillermo A. Perez @ UAntwerp 2021
+Guillermo A. Perez @ UAntwerp, 2022
 """
 
-import csv
+import itertools
+import matplotlib.pyplot as plt
+import pandas as pd
 import sys
 
 
-def parse_csv(filename, bench_root):
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        solve_count = dict()
-        solved_by = dict()
-        for row in csv_reader:
-            if line_count == 0:
-                cols = dict(zip(row, range(len(row))))
-            else:
-                config_name = row[cols["solver"]] + "-" +\
-                    row[cols["configuration"]]
-                bench_name = row[cols["benchmark"]]
-                if config_name not in solve_count:
-                    solve_count[config_name] = 0
-                if bench_name not in solved_by:
-                    solved_by[bench_name] = []
-                solve_count[config_name] += 1
-                solved_by[bench_name].append(row[cols["solver"]] + "-" +
-                                             row[cols["configuration"]])
-            line_count += 1
+def genCactus(filename, parallel=True):
+    bound = "wallclock time" if parallel else "cpu time"
+    if parallel:
+        print("Using time bounds for PARALLEL configurations")
+    else:
+        print("Using time bounds for SEQUENTIAL configurations")
+    # Loading the data in a pandas data frame
+    df = pd.read_csv(filename)
 
-    print("Printing statistics:")
-    unique_solve = [(itm[1][0], itm[0]) for itm in solved_by.items()
-                    if len(itm[1]) == 1]
-    for config, count in sorted(solve_count.items(),
-                                key=lambda itm: itm[1],
-                                reverse=True):
-        print(f"{config} solved {count} benchmarks")
-        unique = [itm[1] for itm in unique_solve
-                  if itm[0] == config]
-        if len(unique) > 0:
-            print(f"{config} uniquely solved {len(unique)} benchs: {unique}")
+    # Remove all entries without a valid output
+    print("Shape of raw input:")
+    print(df.shape[0])
+    df = df[df.result.isin(["REALIZABLE", "NEW-REALIZABLE",
+                            "UNREALIZABLE", "NEW-UNREALIZABLE"])]
+    df = df[~df.status.isin(["timeout (cpu)", "timeout (wallclock)"])]
+    print("Shape after removing invalid output status and timeouts")
+    print(df.shape[0])
 
-    return 0
+    # Prepare to get the best configuration per tool
+    participants = [
+        "ltlsynt",
+        "Otus",
+        "sdf",
+        "SPORE",
+        "Strix",
+        "_acab_sb"
+    ]
+    best = {}
+
+    # Group them by tool configuration for the rest
+    for config, subdf in df.groupby(df.configuration):
+        found = False
+        for p in participants:
+            tool = subdf.head(1).solver.iloc[0]
+            if tool.lower().startswith(p.lower()):
+                found = True
+                subdf = subdf.sort_values(by=bound)
+                cumsum = subdf[bound].cumsum()
+                numbenchs = cumsum.shape[0]
+                if (p not in best or numbenchs > best[p][0]
+                    or (numbenchs == best[p][0] and
+                        cumsum.iloc[-1] < best[p][1].iloc[-1])):
+                    best[p] = (numbenchs, cumsum)
+                break
+        assert(found)
+        print(f"Tool {p}, Configuration {config} solved " +
+              f"{cumsum.shape[0]} benchs " +
+              f"in {cumsum.iloc[-1]}s")
+
+    # Show best plot per tool
+    final = {
+        "Strix PGAME": "Strix",
+        "ltlsynt": "ltlsynt",
+        "Otus": "Otus",
+        "sdf": "sdf",
+        "SPORE": "SPORE",
+        "Strix": "Strix",
+        "_acab_sb": "Acacia bonsai"
+    }
+    markers = itertools.cycle(('h', '+', '.', 'o', '*', 'D', 's'))
+    for tool in best:
+        (_, cumsum) = best[tool]
+        print(f"The best config of {final[tool]} solved {cumsum.shape[0]} " +
+              f"in {cumsum.iloc[-1]}s")
+        plt.plot(range(1, cumsum.shape[0] + 1), cumsum, label=final[tool],
+                 marker=next(markers))
+
+    # Show the plot and close everything after
+    plt.legend(loc="lower right")
+    plt.yscale("log")
+    if parallel:
+        plt.ylabel("Total wall-clock time (s)")
+    else:
+        plt.ylabel("Total cpu time (s)")
+    plt.xlabel("No. of solved benchmarks")
+    plt.show()
+    plt.close()
 
 
 if __name__ == "__main__":
@@ -55,4 +98,6 @@ if __name__ == "__main__":
               + "input file", file=sys.stderr)
         exit(1)
     else:
-        exit(parse_csv(sys.argv[1], None))
+        genCactus(sys.argv[1], False)
+        genCactus(sys.argv[1], True)
+        exit(0)
