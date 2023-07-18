ce0cc2b1e100c4893e896930ffe7c04cf3ed554b
Philipp Schlehuber <philipp.schlehuber@epita.fr>
2023-07-18 07:41:36 +0200

    fix: Detect all Modell checking errors
    Certain Modell checking errors only lead to the exclusion of the
    erroneous instance, not the disqualification of the solver.

--- scripts/inforeal.py
+++ scripts/inforeal.py
@@ -11,40 +11,41 @@ import itertools
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
 
 
 partPerTrack = {
     "PGreal": {
-        "Strix PGAME": "Strix",
-        "Knor repair": "Knor",
+        "strix pgame": "Strix",
+        "knor repair": "Knor",
         "ltlsynt": "ltlsynt"
     },
     "PGsynt": {
-        "Strix PGAME": "Strix",
-        "Knor repair": "Knor",
+        "strix pgame": "Strix",
+        "knor repair": "Knor",
         "ltlsynt": "ltlsynt"
     },
     "LTLreal": {
-        "Strix PGAME": "Strix",
+        "strix pgame": "Strix",
         "ltlsynt23": "ltlsynt",
         "Otus": "Otus",
         "sdf": "sdf",
-        "SPORE": "SPORE",
-        "Strix": "Strix",
+        "spore": "SPORE",
+        "strix": "Strix",
         "abonsai": "Acacia bonsai"
     },
     "LTLsynt": {
         "abonsai": "Acacia bonsai",
         "ltlsynt23": "ltlsynt",
-        "Otus": "Otus",
+        "otus": "Otus",
         "sdf": "sdf",
-        "Strix": "Strix"
+        "strix": "Strix"
     },
     "LTLFreal": {
         "lisa-syntcomp": "lisa",
         "ltlfsynt23prebuild": "ltlfsynt",
-        "LydiaSyft": "LydiaSyft",
-        "Nike": "Nike",
+        "lydiasyft": "LydiaSyft",
+        "nike": "Nike",
         "tople": "tople"
     }
 }
@@ -69,10 +70,27 @@ def genCactus(filename, track, exclude, synthesis, verbose,
     # Loading the data in a pandas data frame
     df = pd.read_csv(filename)
 
+    # If synthesis -> Check the MC result before doing anything else
+    # We need to know if model check errors (not timeouts, errors as in it violates the spec) happened
+    disqualified_solvers = set() # Disqualified solver configurations
+    if synthesis:
+        mcerrors = ["MC-INCORRECT", "MC-ERROR"]
+        errors = df.loc[df.Model_check_result.isin(mcerrors) | df.result.isin(mcerrors), :]
+        if errors.size:
+            initial_jobs = df.shape[0]
+            for (tool, config), subdf in errors.groupby([df.solver, df.configuration]):
+                disqualified_solvers.add((tool, config))
+                print(f"The tool {tool} in configuration {config} failed on instances\n {'; '.join(list(subdf.benchmark))}", file=sys.stderr)
+            # Exclude the corresponding data
+            for (tool, config) in disqualified_solvers:
+                df = df[~((df.solver == tool) & (df.configuration == config))]
+            print(f"A total of {initial_jobs - df.shape[0]} instances have been removed.", file = sys.stderr)
+        else:
+            print("No model checking error detected")
+
     # Remove all entries without a valid output
     bound = "wallclock time" if parallel else "cpu time"
-    print("Shape of raw input:")
-    print(df.shape[0])
+    print(f"Shape of raw input (not counting disqualified):\n{df.shape[0]}")
     print(f"Excluding {len(exclude)} pair ids")
     df = df[~df["pair id"].isin(exclude)]
     possibleRes = ["REALIZABLE", "NEW-REALIZABLE"]
@@ -82,8 +100,7 @@ def genCactus(filename, track, exclude, synthesis, verbose,
     df = df[~df.status.isin(["timeout (cpu)", "timeout (wallclock)"])]
     if timeout is not None:
         df = df[df[bound] <= timeout]
-    print("Shape after removing invalid output status and timeouts")
-    print(df.shape[0])
+    print(f"Shape after removing invalid output status and timeouts:\n{df.shape[0]}")
 
     # Checking for misclassifications
     # this only makes sense if there can be mismatching
@@ -98,7 +115,8 @@ def genCactus(filename, track, exclude, synthesis, verbose,
                 f"{bench} is misclassified by some tool!\n{real}\n{unrl}"
     else:  # for synthesis, check the model-checking result
         fail = df[df["Model_check_result"] != "SUCCESS"]
-        assert fail.shape[0] == 0, f"Model checking failed {fail}"
+        assert fail.shape[0] == 0, f"Model checking failed but tool/config was not excluded"\
+                                   f"or other untreated error in  {fail}"
 
     # Prepare to get information per tool
     participants = partPerTrack[track]
@@ -124,43 +142,39 @@ def genCactus(filename, track, exclude, synthesis, verbose,
 
     # Group them by tool configuration for the rest
     for (tool, config), subdf in df.groupby([df.solver, df.configuration]):
-        found = False
-        for p in participants:
-            if tool.lower().startswith(p.lower()):
-                found = True
-                subdf = subdf.sort_values(by=bound)
-                if verbose:
-                    subdf.to_csv(f"{p}.{config}.csv")
-                numbenchs = subdf.shape[0]
-                cumsum = subdf[bound].cumsum()
-                # store the summary
-                if not synthesis:
-                    summary[(p, config)] = (numbenchs, cumsum.iloc[-1])
-                else:
-                    scoresum = subdf["Synthesis_score"].sum()
-                    summary[(p, config)] = (numbenchs, cumsum.iloc[-1],
-                                            scoresum)
+        assert (tool, config) not in disqualified_solvers, "Table was not properly cleaned"
 
-                # store the info of the best configs
-                if (p not in best or numbenchs > best[p][0]
-                    or (numbenchs == best[p][0] and
-                        cumsum.iloc[-1] < best[p][1].iloc[-1])):
-                    best[p] = (numbenchs, cumsum)
-                print(f"Tool {p}, Configuration {config} solved " +
-                      f"{numbenchs} benchs " +
-                      f"in {cumsum.iloc[-1]}s")
-                break
-        assert found, f"Did not find tool {tool}"
+        tool_name = participants[tool.lower()]
+
+        subdf = subdf.sort_values(by=bound)
+        if verbose:
+            subdf.to_csv(f"{tool_name}.{config}.csv")
+        numbenchs = subdf.shape[0]
+        cumsum = subdf[bound].cumsum()
+        # store the summary
+        if not synthesis:
+            summary[(tool_name, config)] = (numbenchs, cumsum.iloc[-1])
+        else:
+            scoresum = subdf["Synthesis_score"].sum()
+            summary[(tool_name, config)] = (numbenchs, cumsum.iloc[-1], scoresum)
+
+        # store the info of the best configs
+        if (tool_name not in best or numbenchs > best[tool_name][0]
+            or (numbenchs == best[tool_name][0] and
+                cumsum.iloc[-1] < best[tool_name][1].iloc[-1])):
+            best[tool_name] = (numbenchs, cumsum)
+        print(f"Tool {tool_name}, Configuration {config} solved " +
+              f"{numbenchs} benchs " +
+              f"in {cumsum.iloc[-1]}s")
 
     # Show best plot per tool
     markers = itertools.cycle(('h', '+', '.', 'o', '*', 'D', 's'))
-    for tool in best:
-        (_, cumsum) = best[tool]
-        print(f"The best config of {participants[tool]} solved "
+    for tool, (_, cumsum) in best.items():
+        print(f"The best config of {tool} solved "
               f"{cumsum.shape[0]} " +
               f"in {cumsum.iloc[-1]}s")
         plt.plot(range(1, cumsum.shape[0] + 1), cumsum,
-                 label=participants[tool],
+                 label=tool,
                  marker=next(markers))
 
     # Show the plot and close everything after
